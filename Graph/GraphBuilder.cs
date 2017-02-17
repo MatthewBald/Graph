c4cd23796bc59388657b515c39640ec72d59fe75
MatthewBald <matt.e.bald@gmail.com>
2017-02-17 11:37:15 -0500

    Added a GraphBuilder class

---
+++ Graph/GraphBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Graph
+{
+	class GraphBuilder
+	{
+		private Network _network;
+		private FileStream _fin;
+
+		public GraphBuilder(string filePath)
+		{
+			_fin = new FileStream(filePath, FileMode.Open);
+
+		}
+	}
+}
